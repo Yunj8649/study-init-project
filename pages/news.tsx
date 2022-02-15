1e1060c37f453001080977e66205d5ffd70b5e39
yunj <yunj8649@xingxingmobility.com>
2022-02-15 23:25:46 +0900

    뉴스레터 틀잡기

--- pages/news.tsx
+++ pages/news.tsx
@@ -2,9 +2,24 @@ import { FunctionComponent } from 'react'
 
 const News: FunctionComponent = () => {
     return (
-        <>
-            News
-        </>
+        <div>
+            <div>
+                <h2>업계 뉴스</h2>
+                <div>
+                    <div>image</div>
+                    <div>title</div>
+                    <div>date</div>
+                </div>
+            </div>
+            <div>
+                <h2>씽씽 뉴스</h2>
+                <div></div>
+            </div>
+            <div>
+                <h2>그외 뉴스</h2>
+                <div></div>
+            </div>
+        </div>
     );
 }
 
